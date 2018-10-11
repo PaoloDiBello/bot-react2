0ce6e383d42b2d3856229b47bc71d275cdf0474e
Paolo <dibellopol@gmail.com>
2018-10-11 15:55:37 +0200

    new-change2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 import Messages from "./Messages";
 import img from './badge_blue.png';
-import imga from './imga.png';
+/*import imga from './imga.png';*/
 import img2 from './eva_is_listenig.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
- <img src={img} className="circle" data-toggle="modal" data-target="#myModal2"/>    
+ <img src={img} className="circle" data-toggle="modal" data-target="#myModal2" alt="logo"/>    
  <iframe
-        src="http://www.example.com" frameBorder="0" sandbox="allow-scripts allow-same-origin allow-presentation"></iframe>
+        src="http://www.example.com" frameBorder="0" sandbox="allow-scripts allow-same-origin allow-presentation" title="iframe"></iframe>
 
     <div className="container demo">
  {/*
@@ -21,13 +21,13 @@ class App extends Component {
   
    
   {/*-- Modal --*/}
-  <div className="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" data-backdrop="false">
+  <div className="modal right fade" id="myModal2" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel2" data-backdrop="false">
     <div className="modal-dialog" role="document">
       <div className="modal-content">
 
         <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><i class="fas fa-times-circle"></i></span></button>
- <img src={img2}/>    
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><i className="fas fa-times-circle"></i></span></button>
+ <img src={img2} alt="eva_modal"/>    
 {/*
 
           <h4 className="modal-title text-center" id="myModalLabel2">
--- src/Messages.js
+++ src/Messages.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-
-import ReactDOM from 'react-dom';
+import React from 'react';
 import Chat from './chat.json';
 import './Messages.css';
 import { TransitionGroup } from 'react-transition-group' // ES6
-import { CSSTransition } from 'react-transition-group' // ES6
-class ChatBot extends React.Component {
+
+class Messages extends React.Component {
  constructor(props) {
   super(props);
    this.state = {
-  input:'',
-  chat:Chat,
+   input:'',
+   chat:Chat,
   };
  } 
 
-
     handleClick(event) {
     let newObj= [...this.state.chat];
-
     let current_time = new Date(); // for now
     let time=current_time.getHours()+":"+(current_time.getMinutes()<10?'0':'')+current_time.getMinutes();
+    let milliseconds=current_time.getMilliseconds();
     this.setState({ input: '' });  
     /*when the message is not only spaces*/
-    newObj.push({"id":"human","text":this.state.input,"time":time});
+    newObj.push({"id":"human","text":this.state.input,"time":time,"milliseconds":milliseconds});
     this.setState({chat: newObj});  
+
   }
 
   handleChange(event) {
@@ -55,9 +53,9 @@ componentDidUpdate() {
     };
 
   let items=Object.keys(this.state.chat).map((key, i) => {
-  let key_each=this.state.chat[key].text+this.state.chat[key].time;
-    let current_time = new Date(); // for now
-    let time=current_time.getHours()+":"+(current_time.getMinutes()<10?'0':'')+current_time.getMinutes();
+  let current_time = new Date(); // for now
+  let key_each=this.state.chat[key].text+this.state.chat[key].time+this.state.chat[key].milliseconds;
+  let time=current_time.getHours()+":"+(current_time.getMinutes()<10?'0':'')+current_time.getMinutes();
 
 //  console.log(key_each);
   return ( 
@@ -66,8 +64,8 @@ componentDidUpdate() {
     (<div>
     <div className="from-me">
       <p>{this.state.chat[key].text}
-  <span class="metadata">
-  <span class="time">{this.state.chat[key].time===time?"just now":this.state.chat[key].time}</span>
+  <span className="metadata">
+  <span className="time">{this.state.chat[key].time===time?"just now":this.state.chat[key].time}</span>
   </span>
   </p>
 
@@ -76,8 +74,8 @@ componentDidUpdate() {
     (<div key={key_each}>
     <div className="from-them">
       <p>{this.state.chat[key].text}
-      <span class="metadata">
-  <span class="time">{this.state.chat[key].time===time?"just now":this.state.chat[key].time}</span>
+      <span className="metadata">
+  <span className="time">{this.state.chat[key].time===time?"just now":this.state.chat[key].time}</span>
   </span>
   </p>
     </div>
@@ -92,10 +90,10 @@ componentDidUpdate() {
 
           <section>
 
-       <TransitionGroup>
-                {items}
-       </TransitionGroup>
-     
+                  <TransitionGroup enter>
+                    {items}
+                 </TransitionGroup>
+
         <div style={{ float:"left", clear: "both" }}
              ref={(el) => { this.messagesEnd = el; }}>
         </div>
@@ -104,14 +102,14 @@ componentDidUpdate() {
           </section>
 
            <div className="input-group">
-                    <input type="text" class="form-control" value={this.state.input} onChange={this.handleChange.bind(this)} onKeyPress={this._handleKeyPress} placeholder="Type Something..."/>
-                    <span className="input-group-addon" onClick={this.handleClick.bind(this)}><i class="fas fa-location-arrow" style={style_1}></i></span>
+                    <input type="text" className="form-control" value={this.state.input} onChange={this.handleChange.bind(this)} onKeyPress={this._handleKeyPress} placeholder="Type Something..."/>
+                    <span className="input-group-addon" onClick={this.handleClick.bind(this)}><i className="fas fa-location-arrow" style={style_1}></i></span>
                 </div>
         </div>
 
    );
   }
 }
-  export default ChatBot;
+  export default Messages;
  
  
